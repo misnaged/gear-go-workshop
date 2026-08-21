61c96851ff4594afe73a198f3c6b8622ba8983f6
misnaged <pa100ni@gmail.com>
2026-08-21 14:26:07 +0200

    functions    were added

--- internal/gstd/msg.go
+++ internal/gstd/msg.go
@@ -5,3 +5,16 @@ import "github.com/misnaged/gear-go-workshop/internal/gcore"
 func ReplyBytes(payload []byte) {
 	gcore.Reply(payload)
 }
+func LoadBytes() ([]byte, error) {
+	payload := make([]byte, gcore.Size())
+
+	if err := gcore.Read(payload); err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
+
+func Source() gcore.ActorID {
+	return gcore.Source()
+}
--- workshop.go
+++ workshop.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"github.com/misnaged/gear-go-workshop/internal/gstd"
+	msg "github.com/misnaged/gear-go-workshop/internal/gstd"
 )
+
+/*
+//go:wasmexport handle
+func handle() {
+
+	b, err := msg.LoadBytes()
+	if err != nil {
+		return
+	}
+
+	var reply []byte
+
+	switch string(b) {
+	case "beach":
+		reply = []byte("sea")
+
+	case "sand":
+		reply = []byte("desert")
+
+	default:
+		reply = []byte("unknown")
+	}
+
+	msg.ReplyBytes(reply)
+}
+*/
 
 //go:wasmexport handle
 func handle() {
-	msg.ReplyBytes([]byte("PONG"))
+	source := msg.Source()
+
+	msg.ReplyBytes(source[:])
 }
+func main() {}
